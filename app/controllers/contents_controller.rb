4a7e30c931331778e579c5d109d345c735e25c7f
fabrizio_joaquin <fabrizio1908@gmail.com>
2021-11-07 17:03:25 +0100

    aggiunta autorizzazioni e associazioni

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,53 +1,56 @@
 class ContentsController < ApplicationController
   before_action :set_content, only: %i[ show edit update destroy ]
+  
+  before_action :authenticate_user!, except: [:index, :show]
 
-  # GET /contents or /contents.json
+  before_action :check_user, only: [:edit, :update, :destroy]
+
+  
   def index
     @contents = Content.all
   end
 
-  # GET /contents/1 or /contents/1.json
+  
   def show
   end
 
-  # GET /contents/new
+  
   def new
-    @content = Content.new
+    @content = current_user.contents.build
   end
 
-  # GET /contents/1/edit
+
   def edit
   end
 
-  # POST /contents or /contents.json
+  
   def create
-    @content = Content.new(content_params)
+    @content = current_user.contents.build(content_params)
 
     respond_to do |format|
       if @content.save
-        format.html { redirect_to @content, notice: "Content was successfully created." }
-        format.json { render :show, status: :created, location: @content }
+        format.html { redirect_to @content, notice: "Content was successfully created." } 
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @content.errors, status: :unprocessable_entity }
+        
       end
     end
   end
 
-  # PATCH/PUT /contents/1 or /contents/1.json
+  
   def update
     respond_to do |format|
       if @content.update(content_params)
         format.html { redirect_to @content, notice: "Content was successfully updated." }
-        format.json { render :show, status: :ok, location: @content }
+       
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @content.errors, status: :unprocessable_entity }
+        
       end
     end
   end
 
-  # DELETE /contents/1 or /contents/1.json
+  
   def destroy
     @content.destroy
     respond_to do |format|
@@ -57,13 +60,19 @@ class ContentsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+   
     def set_content
       @content = Content.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
+    
     def content_params
       params.require(:content).permit(:titolo, :descrizione, :price)
     end
+
+      def check_user
+        if current_user != @content.user
+          redirect_to root_url, alert: "Scusa ma non hai accesso a questa pagina"
+        end
+      end
 end
